3effad860edb4ea1e7d074c97a2ae2f8fdb7ddb7
xunyss <xunyss@gmail.com>
2015-03-18 18:10:59 +0900

    init

---
+++ jasca-pmd/src/main/java/com/tyn/jasca/pmd/rule/ReverseDNSResolutionRule.java
@@ -0,0 +1,35 @@
+package com.tyn.jasca.pmd.rule;
+
+import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTName;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+
+/**
+ * 
+ * @author S.J.H.
+ */
+public class ReverseDNSResolutionRule extends AbstractJavaRule {
+
+	@Override
+	public Object visit(ASTPrimaryExpression node, Object data) {
+		
+		ASTPrimaryPrefix prefix = (ASTPrimaryPrefix) node.jjtGetChild(0);
+		Node name = prefix.jjtGetChild(0);
+		
+		if (name instanceof ASTName) {
+			if (name.getImage().endsWith("getCanonicalHostName")) {
+				
+				// TODO
+				// type check
+				// arguments check
+				
+				// violation
+				addViolation(data, node);
+			}
+		}
+		
+		return data;
+	}
+}
